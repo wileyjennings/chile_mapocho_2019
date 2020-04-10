4e10091d88c11a92b15fec43ab2300451a1cf584
wileyjennings <wileyjennings@gmail.com>
2020-04-10 16:11:30 -0700

    Fixed qPCR target naming convention

--- scripts/util.R
+++ scripts/util.R
@@ -1,14 +1,14 @@
 
 # Requirements ------------------------------------------------------------
 
-required_packages <- c("here", "tidyverse")
+required_packages <- c("dplyr", "here", "purrr", "readxl", "stringr")
 lapply(required_packages, library, character.only = T)
 
 
 # Reads qPCR data files in default format produced by Step One Plus qPCR platform.
 
 # Given:
-# - target: the qPCR target. Can be "HF183", "crass", or "noro"
+# - target: the qPCR target. Can be "HF183", "crAssphage", or "noroGII"
 # --  a string
 # Returns:
 # - A list of tibbles, where each element is the results from one instrument
@@ -21,9 +21,16 @@ lapply(required_packages, library, character.only = T)
 # -- Contains date formatted as YYYYMMDD.
 
 read_s1plus <- function(target_) {
-  file_name <- paste0(target_, "*.xls")
-  path <- here::here("data", "raw", "qPCR", target_, "results", 
-                     paste0(target_,"*.xls"))
+  if (target_ == "hf183") {
+    path_target <- "HF183"
+  } else if (target_ == "crass") {
+    path_target <- "crAssphage"
+  } else if (target_ == "noro") {
+    path_target <- "noroGII"
+  } else {stop("Target must be 'hf183', 'crass', or 'noro'")}
+  file_name <- paste0(path_target, "*.xls")
+  path <- here::here("data", "raw", "qPCR", path_target, "results", 
+                     paste0(path_target,"*.xls"))
   paths <- Sys.glob(path)
   
   # Read results sheets from qPCR workbook files
@@ -34,11 +41,7 @@ read_s1plus <- function(target_) {
   tib_list <- map2(.x = tib_list, .y = dates, ~ mutate(.x, date = .y)) 
   
   # Add plate name and target to each plate run 
-  if (target == "HF183") {
-    start_ <- 99
-  } else if (target %in% c("crass", "noro")) {
-    start_ <- 104
-  } else {stop("Target must be 'HF183', 'crass', or 'noro'")}
+  start_ <- ifelse(target_ == "hf183", 99, 104)
   plate_names <- mapply(FUN = substr, x = paths, start = start_, 
                         stop = nchar(paths)-4)
   tib_list <- map2(.x = tib_list, .y = plate_names, ~ mutate(.x, plate_name = .y)) 
