5bd7950526f59686387d1839dd0f88cdcf7f8512
wileyjennings <wileyjennings@gmail.com>
2020-04-24 13:07:27 -0700

    Standards summary write location should be results not figures

--- scripts/02_estim_qpcr_stnd_eqns.R
+++ scripts/02_estim_qpcr_stnd_eqns.R
@@ -64,5 +64,5 @@ standards_plot <-
 saveRDS(standards_mixed_summary, 
         here::here("data", "processed", "standards_mixed_summary.rds"))
 saveRDS(standards_mixed, here::here("data", "processed", "standards_mixed.rds"))
-write_csv(standards_mixed_summary, here::here("figures", "standards_summary.csv"))
+write_csv(standards_mixed_summary, here::here("results", "standards_summary.csv"))
 write_tif(standards_plot, "standards.tif")
