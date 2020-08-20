38a8b13e3250b359f37618780d57a8a645ca49ca
wileyjennings <wileyjennings@gmail.com>
2020-08-20 07:52:02 -0400

    Added LOD, LOQ, and LME R2 functions

--- scripts/02_estim_qpcr_stnd_eqns.R
+++ scripts/02_estim_qpcr_stnd_eqns.R
@@ -28,29 +28,96 @@ standards <- qpcr %>% filter(task == "STANDARD")
 # Estimate mixed standard model (assuming random intercept).
 standards_mixed <- estim_std_mix(standards)
 standards_mixed_summary <- summarize_std(standards_mixed)
+# Note: the condition R2 for a mixed model is different from R2 for lm. It 
+# assumes that explained variance includes both fixed and random effects, 
+# compared to residual variance
 
 
 # Estimate LOD and LOQ ----------------------------------------------------
 
 # LOD: the theoretical LOD from Bustin 2009: 3 cp/rxn, per poisson sub-sampling
-lod <- tibble(target = c("crass", "hf183", "noro"), lod_l10 = log10(3))
+# lod <- tibble(target = c("crass", "hf183", "noro"), lod_l10 = log10(3))
+
+lod_crass <- lod_logit(standards, "crass", .prob = 0.95)
+lod_hf183 <- lod_logit(standards, "hf183", .prob = 0.95)
+lod_noro <- lod_logit(standards, "noro", .prob = 0.95)
+
+loq_inspect_crass <- lloq_pool(standards, "crass", 
+                       .eff = 
+                         standards_mixed_summary %>% 
+                         filter(target == "crass") %>%
+                         pull(effic))
+loq_inspect_hf183 <- lloq_pool(standards, "hf183", 
+                       .eff = 
+                         standards_mixed_summary %>% 
+                         filter(target == "hf183") %>%
+                         pull(effic))
+loq_inspect_noro <- lloq_pool(standards, "noro", 
+                       .eff = 
+                         standards_mixed_summary %>% 
+                         filter(target == "noro") %>%
+                         pull(effic))
+
+# Based on inspection, noro lOD is used as LOQ because coefficient of variation
+# never exceeds 0.35, and LOQ cannot be less than LOD
+lod_loq <- tibble(
+  target = c("crass", "hf183", "noro"),
+  lod_l10 = c(lod_crass, lod_hf183, lod_noro),
+  loq_l10 = c(loq_inspect_crass$loq_l10, loq_inspect_hf183$loq_l10, 
+          lod_noro)) 
+# If LOQ < LOD, assigned LOD to LOQ
+lod_loq <- 
+  lod_loq %>%
+  mutate(loq_l10 = ifelse(loq_l10 < lod_l10, lod_l10, loq_l10))
+
+
+# crass_spline <- smooth.spline(x = log10(loq_crass$copy_num),
+#                               y = loq_crass$cv,
+#                               spar = 0.3)
+# spline_pred <-
+#   predict(crass_spline,
+#           x = seq(min(log10(loq_crass[[1]]$copy_num)),
+#                   max(log10(loq_crass[[1]]$copy_num)),
+#                   0.1)) %>%
+#   as_tibble()
+# names(spline_pred) <- c("copy_num_l10", "cv_spline_fit")
+# spline_pred$deriv <- predict(crass_spline,
+#           x = seq(min(log10(loq_crass[[1]]$copy_num)),
+#                   max(log10(loq_crass[[1]]$copy_num)),
+#                   0.1), 
+#           deriv = 1)$y
+# spline_pred %>%
+#   filter(cv_spline_fit < 0.35, deriv < 0) %>%
+#   summarize(loq_l10 = min(copy_num_l10)) %>%
+#   pull(loq_l10)
+# 
+# # plot_test <-
+#   loq_crass %>%
+#   ggplot(., aes(x = log10(copy_num), y = cv)) +
+#   geom_point() +
+#   geom_line(data = spline_pred, aes(x = x, y = y)) +
+#   # geom_smooth(method = 'loess', formula = 'y ~ x', se = F) +
+#   geom_hline(aes(yintercept = 0.35))
+# test <- list(
+#   loq_crass = loq_crass,
+#   plot_test = plot_test,
+#   spline_pred = spline_pred)
 
 # LLOQ: use logistic regression to estimate concentration at which 50% of samples
 # show amplification.
-loq_crass <- lloq_logit(standards, "crass", .prob = 0.5)
-loq_hf183 <- lloq_logit(standards, "hf183", .prob = 0.5)
-loq_noro <- lloq_logit(standards, "noro", .prob = 0.5)
+# loq_crass <- lloq_logit(standards, "crass", .prob = 0.5)
+# loq_hf183 <- lloq_logit(standards, "hf183", .prob = 0.5)
+# loq_noro <- lloq_logit(standards, "noro", .prob = 0.5)
 
 # Assign noro lloq to be log10(3), because of estimation problem and estimated
 # lloq < lod = 3
-loq <- tibble(target = c("crass", "hf183", "noro"),
-              loq_l10 = c(loq_crass, loq_hf183, log10(3)))
+# loq <- tibble(target = c("crass", "hf183", "noro"),
+#               loq_l10 = c(loq_crass, loq_hf183, log10(3)))
 
 # Add LOD and LOQ to summary of mixed standards.
 standards_mixed_summary <- 
   standards_mixed_summary %>%
-  left_join(., lod, by = "target") %>%
-  left_join(., loq %>% select(loq_l10, target), by = "target")
+  left_join(., lod_loq, by = "target")
 
 # Plot standards. This plots all across plates.
 standards_plot <- 
@@ -66,3 +133,6 @@ saveRDS(standards_mixed_summary,
 saveRDS(standards_mixed, here::here("data", "processed", "standards_mixed.rds"))
 write_csv(standards_mixed_summary, here::here("results", "standards_summary.csv"))
 write_tif(standards_plot, "standards.tif")
+write_tif(loq_inspect_crass$loq_plot, "loq_crass.tif")
+write_tif(loq_inspect_hf183$loq_plot, "loq_hf183.tif")
+write_tif(loq_inspect_noro$loq_plot, "loq_noro.tif")
